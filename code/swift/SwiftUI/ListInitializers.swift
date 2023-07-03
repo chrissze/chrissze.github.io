06f4123d151862faeeb04c1d14877ef2d6eb4ac3
Chris Sze <8147803+gate-max@users.noreply.github.com>
2023-07-03 23:38:48 +0800

    Update ListInitializers.swift

--- code/swift/SwiftUI/ListInitializers.swift
+++ code/swift/SwiftUI/ListInitializers.swift
@@ -1,1 +1,30 @@
 
+// I could copy and paste this file's content to replace any new SwiftUI project's ContentView.swift
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            SimpleListView()
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+struct SimpleListView: View {
+    var body: some View {
+        List {
+            Text("Appleseed")
+            Text("Grapes")
+            Text("Strawberry")
+        }
+}
+
+    
