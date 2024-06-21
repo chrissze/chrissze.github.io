466a542c20fa6861dbc23b77eb3d23aae3843979
Chris Sze <8147803+chrissze@users.noreply.github.com>
2024-06-21 13:21:40 +0800

    Update addaccounts.sh

--- docker/mailserver/addaccounts.sh
+++ docker/mailserver/addaccounts.sh
@@ -1,6 +1,24 @@
-./setup.sh email add cs@nutdb.com
+
+./setup.sh   # show options
+
+
+./setup.sh email add cs@nutdb.com  # will prompt for pw
+
+./setup.sh email update cs@nutdb.com  # update pw
+
+
+./setup.sh email del xxx@nutdb.com 
 
 
 ./setup.sh email list
 
-./setup.sh alias list
+
+
+
+./setup.sh alias add virtual@220122.xyz real@220122.xyz
+
+./setup.sh alias del virtual@220122.xyz real@220122.xyz
+
+
+./setup.sh alias list    # SHOWS virtual@220122.xyz real@220122.xyz
+
