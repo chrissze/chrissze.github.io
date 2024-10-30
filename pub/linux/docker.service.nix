4be15ed7c1d56780c37fc25b9dde5fd063bf8889
Chris Sze <8147803+chrissze@users.noreply.github.com>
2024-10-30 16:34:22 +0800

    Create docker.service.nix

---
+++ pub/linux/docker.service.nix
@@ -0,0 +1,30 @@
+[Unit]
+Description=Docker Application Container Engine
+Documentation=https://docs.docker.com
+After=docker.socket network-online.target firewalld.service
+Requires=docker.socket
+Wants=network-online.target
+
+[Service]
+Type=notify
+ExecStart=/home/cs/.nix-profile/bin/dockerd \
+          --host=fd:// \
+          --exec-opt native.cgroupdriver=systemd \
+          $OPTIONS
+
+ExecReload=/bin/kill -s HUP $MAINPID
+LimitNOFILE=infinity
+LimitNPROC=infinity
+LimitCORE=infinity
+
+TimeoutStartSec=0
+# set delegate yes so that systemd does not reset the cgroups of docker containers
+#Delegate=yes
+KillMode=process
+
+Restart=on-failure
+StartLimitBurst=3
+StartLimitInterval=60s
+
+[Install]
+WantedBy=multi-user.target
