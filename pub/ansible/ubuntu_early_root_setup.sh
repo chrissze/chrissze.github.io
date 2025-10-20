2769b153d47ea90c9450a1410b46c287522088eb
Chris Sze <3chris@gmail.com>
2025-10-20 10:38:48 +0800

    Update ubuntu_early_root_setup.sh

--- pub/ansible/ubuntu_early_root_setup.sh
+++ pub/ansible/ubuntu_early_root_setup.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
+# Both Ubuntu and Debian support 'apt full-upgrade', it will aggressively remove packages for resolutions, but 'apt upgrade' will NOT remove obsolete packages. 
+
+
 apt update && apt upgrade -y && apt autoremove -y && apt clean
 
 apt install ansible -y 
